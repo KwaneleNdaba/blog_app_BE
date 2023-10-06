7dc2682228703fc071d9f3f3bb8e5a29f7906bf7
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2023-10-06 04:01:40 +0200

    access control orifin

--- src/index.js
+++ src/index.js
@@ -9,10 +9,22 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 
+const cors = require('cors'); 
+
 dotenv.config();
 
 app.use(express.json()); // App should be able to send JSON data
 
+app.use(function(req, res, next) {
+  app.use(cors({ origin: 'http://localhost:3000' }));
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Request-Headers",
+  "access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,authorization,content-type,access-control-allow-origin");
+   res.header("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers,access-control-allow-origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  
+    next();
+    });
 // Update MONGO_URL to include the "blogs" database
 mongoose.connect(process.env.MONGO_URL + "blogs", {
   useNewUrlParser: true,
